ccd45dc91e62114456e651090006f3957db8edf8
ayaan-d <ayaan0145@gmail.com>
2023-01-19 20:13:47 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import {useState} from 'react';
 import React from "react";
+import { BsArrowLeftShort } from "react-icons/bs";
+import { AiFillEnvironment } from "react-icons/ai";
+
 
 const App = () => {
+  const [open, setOpen] = useState(true);
+
   return (
-    <div className="max-w-[1240px] p-4 flex m-auto pt-12">
-      <h1 className="font-bold text-[#bf3edc]">Ayaan, time for you to learn React!</h1>
+    <div className="flex">
+      <div 
+      className={`bg-grey h-screen p-5 pt-8 rounded-r-lg
+      ${open ? "w-[9%]" : "w-[15%]"} duration-300 relative`}
+      >
+        <BsArrowLeftShort 
+        className={`bg-blue text-grey text-3xl rounded-full 
+        absolute -right-3 top-9 border border-grey 
+        cursor-pointer 
+        ${!open && "rotate-180"
+        }`} 
+        onClick={() => setOpen(!open)}
+      />
+      <div className="inline-flex">
+        <AiFillEnvironment className={`bg-amber-300 text-4xl 
+        rounded cursor-pointer float-left duration-500 lg:mr-[25%] sm:mr-[40%] md:mr-[30%]
+        hidden md:lg:block
+        ${open && "rotate-[360deg]"}`} />
+      </div>
+      </div>
+      <div className="p-7">
+        <h1 className="text-2xl font-semibold"> Home Page</h1>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: '#cbd5e1',
+        blue: '#334155', 
+      }
+    },
   },
   plugins: [],
 }
